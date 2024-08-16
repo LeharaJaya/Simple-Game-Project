6db68fbeb5641dcfc3d2c187511a2d8202deda82
Chamee Jayasena <leharachamee20@gmail.com>
2024-08-16 21:58:53 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+     Player1 player = new Player1("Sam","sword",100);
+
+     player.damageByGun1();
+     player.damageByGun2();
+     player.heal();
+
+    }
+
+}
